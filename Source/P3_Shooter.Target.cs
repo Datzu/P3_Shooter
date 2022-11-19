faa890295292322b285809fa943462e29c28b988
Adrián <murkhies@gmail.com>
2022-11-19 21:34:07 +0100

    Commit

--- Source/P3_Shooter.Target.cs
+++ Source/P3_Shooter.Target.cs
@@ -1,15 +1,14 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class P3_ShooterTarget : TargetRules
 {
-	public P3_ShooterTarget(TargetInfo Target) : base(Target)
+	public P3_ShooterTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-
 		ExtraModuleNames.AddRange( new string[] { "P3_Shooter" } );
 	}
 }
--- Source/P3_ShooterEditor.Target.cs
+++ Source/P3_ShooterEditor.Target.cs
@@ -1,15 +1,14 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class P3_ShooterEditorTarget : TargetRules
 {
-	public P3_ShooterEditorTarget(TargetInfo Target) : base(Target)
+	public P3_ShooterEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-
 		ExtraModuleNames.AddRange( new string[] { "P3_Shooter" } );
 	}
 }
